6c5386fa227f54ac82d966bd7ae01ae7524df602
BoomslangRU <eilinals@mail.ru>
2021-03-23 20:16:53 +0300

    working with a file

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+let inputResult = document.querySelector('#input-result-tabl');
+inputResult.value = '0';
+let numberOne;
+let numberTwo;
 
 let numberButtons = document.querySelectorAll('.number-button');
-
 for (let i = 0; i < numberButtons.length; i++) {
-    let numberButtonsResult = numberButtons[i];
-        numberButtonsResult.addEventListener('click', numberButtonClickListener);
+    const numberButtonsArray = numberButtons[i];
+    numberButtonsArray.addEventListener('click', numberButtonClickListener);
+}
+
+let operationButtons = document.querySelectorAll('.operation-button');
+for (let i = 0; i < operationButtons.length; i++) {
+    const operationButtonsArray = operationButtons[i];
+    operationButtonsArray.addEventListener('click', operationButtonClickListener);
 }
 
 function numberButtonClickListener(e) {
-    let inputResult = document.querySelector('#input-result-tabl');
-    inputResult.value = e.currentTarget.value;
+    let integer = inputResult.value
+    numberOne = e.currentTarget.value;
+    inputResult.value = numberOne;
+    if (integer != '0') { 
+        inputResult.value = integer + numberOne; 
+    }else{
+        inputResult.value = numberOne;
+    }
+    
 }
-
 
