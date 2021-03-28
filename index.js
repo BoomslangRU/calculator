e406573c0f8737122c2ef58d6d67eb883beef08e
BoomslangRU <eilinals@mail.ru>
2021-03-28 16:29:42 +0300

    adding classes to tags

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// BUSINESS CODE
 let inputResult = document.querySelector('#input-result-tabl');
 inputResult.value = '0';
-let numberOne;
-let numberTwo;
+let firstNumber;
+let lastNumber;
+let mathematicalOperation;
+let finalResult = '0';
 
 let numberButtons = document.querySelectorAll('.number-button');
 for (let i = 0; i < numberButtons.length; i++) {
@@ -15,15 +18,77 @@ for (let i = 0; i < operationButtons.length; i++) {
     operationButtonsArray.addEventListener('click', operationButtonClickListener);
 }
 
+
+
 function numberButtonClickListener(e) {
-    let integer = inputResult.value
-    numberOne = e.currentTarget.value;
-    inputResult.value = numberOne;
-    if (integer != '0') { 
-        inputResult.value = integer + numberOne; 
-    }else{
-        inputResult.value = numberOne;
+    let theInteger = inputResult.value;
+    let theNumber = e.currentTarget.value;
+    if (mathematicalOperation === undefined) {
+        if (theInteger != '0' ) {
+            inputResult.value = theInteger + theNumber;
+        } else {
+            inputResult.value = theNumber;
+        }
+        firstNumber = inputResult.value;
+    } else {
+        if (theInteger != '0') {
+            inputResult.value = theInteger + theNumber;
+        } else {
+            inputResult.value = theNumber;
+        }
+        lastNumber = inputResult.value;
     }
-    
 }
 
+
+
+
+function operationButtonClickListener(e) {
+    theOperation = e.currentTarget.value;
+    inputResult.value = null;
+    if (mathematicalOperation === undefined || theOperation === "C") {
+        resultOperation(theOperation);
+    } else {
+        finalResultCalculator();
+        resultOperation(theOperation);
+    }
+}
+
+function resultOperation(theOperation) {
+    mathematicalOperation = theOperation;
+    if (theOperation === 'C') {
+        resetValues();
+    }
+}
+
+function finalResultCalculator() {
+    if (mathematicalOperation == 'X') {
+        finalResult = firstNumber * lastNumber;
+    } else if (mathematicalOperation == '-') {
+        finalResult = firstNumber - lastNumber;
+    } else if (mathematicalOperation == '+') {
+        finalResult = +firstNumber + +lastNumber;
+    } else if (mathematicalOperation == '/') {
+        finalResult = firstNumber / lastNumber;
+    } else if (mathematicalOperation == 'C') {
+        resetValues();
+    } else if (mathematicalOperation == '=') {
+        inputResult.value = finalResult;
+    } else {
+        console.log('calculator error');
+    }
+    inputResult.value = finalResult;
+}
+
+function resetValues() {
+    inputResult.value = '0';
+    firstNumber = undefined;
+    lastNumber = undefined; 
+    mathematicalOperation = undefined;
+    finalResult = '0';
+}
+
+
+
+
+// UI CODE
